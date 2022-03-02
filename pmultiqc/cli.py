1288736441398f009f4317ebd52db9dd496c628e
chengxin Dai <37200167+daichengxin@users.noreply.github.com>
2022-03-02 20:35:25 +0800

    update pmultiqc  -> 0.0.10

--- pmultiqc/cli.py
+++ pmultiqc/cli.py
@@ -10,27 +10,29 @@ http://click.pocoo.org/5/
 import click
 from multiqc.utils import config
 
-def print_version(ctx, param, value):
+
+def print_version(ctx, params, value):
     if not value or ctx.resilient_parsing:
         return
     click.echo("pmultqic, version " + config.pmultiqc_version)
     ctx.exit()
 
+
 exp_design = click.option('--exp_design', 'exp_design')
-pmultiqc_version = click.option('--pmultiqc_version', is_flag=True, callback=print_version, expose_value=False, is_eager=True)
+pmultiqc_version = click.option('--pmultiqc_version', is_flag=True, callback=print_version, expose_value=False,
+                                is_eager=True)
 sdrf = click.option('--sdrf', 'sdrf', help='The path of sdrf file')
 raw = click.option('--raw', 'raw', help='Keep filenames in experimental design output as raw.', default=False)
 condition = click.option('--condition', "condition",
-                            help='Create conditions from provided (e.g., factor) columns.')
-quant_method = click.option('--quant_method', 'quant_method', help='LFQ or ISO', default='LFQ')
+                         help='Create conditions from provided (e.g., factor) columns.')
 mzMLs = click.option('--mzMLs', 'mzMLs', default="mzMLs/", help='The directory of mzML files')
 raw_ids = click.option('--raw_ids', 'raw_ids', default="raw_ids/", help="The directory of idXML files")
 remove_decoy = click.option('--remove_decoy', 'remove_decoy', default=True)
 decoy_affix = click.option('--decoy_affix', 'decoy_affix', default='DECOY_',
-                            help='The decoy prefix or suffix used or to be used (default: DECOY_)')
+                           help='The decoy prefix or suffix used or to be used (default: DECOY_)')
 affix_type = click.option('--affix_type', 'affix_type', default='prefix',
-                            help='Prefix (default) or suffix')
+                          help='Prefix (default) or suffix')
 disable_plugin = click.option('--disable_plugin', 'disable_plugin',
-                                is_flag=True,
-                                help="Disable the pmultiqc plugin on this run"
-                            )
+                              is_flag=True,
+                              help="Disable the pmultiqc plugin on this run"
+                              )
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ For more information about MultiQC, see http://multiqc.info
 
 from setuptools import setup, find_packages
 
-version = '0.0.9'
+version = '0.0.10'
 
 
 def readme():
@@ -32,10 +32,11 @@ setup(
     install_requires=[
         'lxml',
         'multiqc',
-        'pandas',
+        'pandas==1.3.4',
         'pyteomics',
         'sdrf-pipelines >= 0.0.18',
-        'numpy'
+        'numpy',
+        'pyopenms'
     ],
     entry_points={
         'multiqc.modules.v1': [
@@ -48,7 +49,6 @@ setup(
             'sdrf = pmultiqc.cli:sdrf',
             'raw = pmultiqc.cli:raw',
             'condition = pmultiqc.cli:condition',
-            'quant_method = pmultiqc.cli:quant_method',
             'mzMLs = pmultiqc.cli:mzMLs',
             'raw_ids = pmultiqc.cli:raw_ids',
             'remove_decoy = pmultiqc.cli:remove_decoy',
