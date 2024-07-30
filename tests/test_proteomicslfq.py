65fa9d1c1c021cda3d123d5ed9aa88a7194e9547
Yasset Perez-Riverol <ypriverol@gmail.com>
2024-07-30 18:02:26 +0100

    multiqc 1.19.0 -> 1.23

--- tests/test_proteomicslfq.py
+++ tests/test_proteomicslfq.py
@@ -1,9 +1,7 @@
 from unittest import TestCase
 
-from pmultiqc.modules.quantms import QuantMSModule
-
-
 class TestMultiqcModule(TestCase):
   path_mztab1 = 'resources/PXD005942-Sample-25-out.mzTab'
   def test_parse_out_mz_tab(self):
     print("Improve tests here")
+
