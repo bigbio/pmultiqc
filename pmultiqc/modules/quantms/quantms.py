8e8cbb93a346976df4667e630f570547f0ce6530
Qi-Xuan Yue <yueqx@foxmail.com>
2025-05-07 22:10:29 +0800

    add MaxQuant HeatMap

--- pmultiqc/modules/quantms/quantms.py
+++ pmultiqc/modules/quantms/quantms.py
@@ -26,6 +26,7 @@ import sqlite3
 import numpy as np
 import copy
 import json
+from matplotlib.colors import to_hex, LinearSegmentedColormap
 
 from . import sparklines
 from .ms_functions import get_ms_qc_info
@@ -82,6 +83,10 @@ class QuantMSModule(BaseMultiqcModule):
         # Halt execution if we've disabled the plugin
         if config.kwargs.get("disable_plugin", True):
             return None
+
+        # HeatMap color list
+        color_map = LinearSegmentedColormap.from_list("red_green", ["#ff0000", "#00ff00"])
+        self.heatmap_color_list = [[s, to_hex(color_map(s))] for s in [round(i * 0.1, 1) for i in range(11)]]
 
         # Parse MaxQuant results
         if config.kwargs.get("parse_maxquant", False):
@@ -227,6 +232,15 @@ class QuantMSModule(BaseMultiqcModule):
             # Parameters
             if get_parameter_dicts["parameters_tb_dict"]:
                 self.draw_parameters(get_parameter_dicts["parameters_tb_dict"])
+
+            # HeatMap
+            heatmap_data = maxquant.calculate_heatmap(
+                evidence_df=get_evidence_dicts["evidence_df"],
+                oversampling=get_evidence_dicts["oversampling"]["plot_data"],
+                msms_missed_cleavages=get_msms_dicts["missed_cleavages"]["plot_data"]
+                )
+            if heatmap_data:
+                self.draw_maxquant_heatmap(heatmap_data)
 
             # Intensity
             if get_protegroups_dicts["pg_intensity_distri"]:
@@ -593,10 +607,15 @@ class QuantMSModule(BaseMultiqcModule):
 
         pconfig = {
             "id": "HeatMap",
-            "title": "Performance Overview",  # Plot title - should be in format "Module Name: Plot Title"
-            "xlab": "metrics",  # X-axis title
-            "ylab": "RawName",  # Y-axis title
+            "title": "Performance Overview",
+            "min": 0,
+            "max": 1,
+            "xlab": "Metrics",
+            "ylab": "RawName",
+            "zlab": "Score",
+            "tt_decimals": 4,
             "square": False,
+            "colstops": self.color_list,
         }
 
         hm_html = heatmap.plot(heat_map_score, xnames, ynames, pconfig)
@@ -616,9 +635,9 @@ class QuantMSModule(BaseMultiqcModule):
                         Raw file (median). For typtic digests, peptides of charge 2 (one N-terminal and one at 
                         tryptic C-terminal R or K residue) should be dominant. Ionization issues (voltage?), 
                         in-source fragmentation, missed cleavages and buffer irregularities can cause a shift 
-                        (see Bittremieux 2017, DOI: 10.1002/mas.21544 ).
-                    * Heatmap score [MC]: the fraction (0% - 100%) of fully cleaved peptides per Raw file
-                    * Heatmap score [MC Var]: each Raw file is scored for its deviation from the ‘average’ digestion 
+                        (see Bittremieux 2017, DOI: 10.1002/mas.21544).
+                    * Heatmap score [Missed Cleavages]: the fraction (0% - 100%) of fully cleaved peptides per Raw file
+                    * Heatmap score [Missed Cleavages Var]: each Raw file is scored for its deviation from the ‘average’ digestion 
                         state of the current study.
                     * Heatmap score [ID rate over RT]: Judge column occupancy over retention time. 
                         Ideally, the LC gradient is chosen such that the number of identifications 
@@ -630,7 +649,7 @@ class QuantMSModule(BaseMultiqcModule):
                         identified by at least two distinct MS2 spectra in the same Raw file. For high complexity samples, 
                         oversampling of individual 3D-peaks automatically leads to undersampling or even omission of other 3D-peaks, 
                         reducing the number of identified peptides.
-                    * Heatmap score [Pep Missing]: Linear scale of the fraction of missing peptides.
+                    * Heatmap score [Pep Missing Values]: Linear scale of the fraction of missing peptides.
                     
                     """,
             plot=hm_html,
@@ -3502,6 +3521,63 @@ class QuantMSModule(BaseMultiqcModule):
                 maxquant_paths[self.file_prefix(f_path)] = f_path
 
         return maxquant_paths
+
+    # MaxQuant HeatMap
+    def draw_maxquant_heatmap(self, heatmap_data):
+
+        pconfig = {
+            "id": "heatmap",
+            "title": "Performance Overview",
+            "min": 0,
+            "max": 1,
+            "xlab": "Metrics",
+            "ylab": "RawName",
+            "zlab": "Score",
+            "tt_decimals": 4,
+            "square": False,
+            "xcats_samples": False,
+            "ycats_samples": False,
+            "colstops": self.heatmap_color_list,
+        }
+    
+        hm_html = heatmap.plot(data=heatmap_data, pconfig=pconfig)
+
+        # Add a report section with the heatmap plot
+        self.add_section(
+            name="HeatMap",
+            anchor="maxquant_heatmap",
+            description="This heatmap provides an overview of the performance of the MaxQuant results.",
+            helptext="""
+                    This plot shows the pipeline performance overview. Some metrics are calculated.
+                    
+                    * Heatmap score[Contaminants]: as fraction of summed intensity with 0 = sample full of contaminants; 
+                        1 = no contaminants
+                    * Heatmap score[Pep Intensity (>23.0)]: Linear scale of the median intensity reaching the threshold, 
+                        i.e. reaching 2^21 of 2^23 gives score 0.25.
+                    * Heatmap score[Charge]: Deviation of the charge 2 proportion from a representative 
+                        Raw file (median). For typtic digests, peptides of charge 2 (one N-terminal and one at 
+                        tryptic C-terminal R or K residue) should be dominant. Ionization issues (voltage?), 
+                        in-source fragmentation, missed cleavages and buffer irregularities can cause a shift 
+                        (see Bittremieux 2017, DOI: 10.1002/mas.21544).
+                    * Heatmap score [Missed Cleavages]: the fraction (0% - 100%) of fully cleaved peptides per Raw file
+                    * Heatmap score [Missed Cleavages Var]: each Raw file is scored for its deviation from the ‘average’ digestion 
+                        state of the current study.
+                    * Heatmap score [ID rate over RT]: Judge column occupancy over retention time. 
+                        Ideally, the LC gradient is chosen such that the number of identifications 
+                        (here, after FDR filtering) is uniform over time, to ensure consistent instrument duty cycles. 
+                        Sharp peaks and uneven distribution of identifications over time indicate potential for LC gradient 
+                        optimization.Scored using ‘Uniform’ scoring function. i.e. constant receives good score, extreme shapes are bad.
+                    * Heatmap score [MS2 Oversampling]: The percentage of non-oversampled 3D-peaks. An oversampled 
+                        3D-peak is defined as a peak whose peptide ion (same sequence and same charge state) was 
+                        identified by at least two distinct MS2 spectra in the same Raw file. For high complexity samples, 
+                        oversampling of individual 3D-peaks automatically leads to undersampling or even omission of other 3D-peaks, 
+                        reducing the number of identified peptides.
+                    * Heatmap score [Pep Missing Values]: Linear scale of the fraction of missing peptides.
+                    
+                    """,
+            plot=hm_html,
+        )
+
 
     # MaxQuant parameters table
     def draw_parameters(self, parameter_table):
