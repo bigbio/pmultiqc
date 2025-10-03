5d067836757e54750cf9148c23f831bbf7ad4d05
Yasset Perez-Riverol <ypriverol@gmail.com>
2025-10-03 20:17:16 +0100

    black added

--- tests/test_statistics_utils.py
+++ tests/test_statistics_utils.py
@@ -14,14 +14,14 @@ class TestStatisticsUtils:
 
         ## no NaN's
         a = np.array([10.0, 7, 4, 3, 2, 1])
-        assert pm_nanmedian(a, 0)==3.5, "nanmedian() on just numbers failed"
+        assert pm_nanmedian(a, 0) == 3.5, "nanmedian() on just numbers failed"
 
         ## a few NaNs (fallback not required)
         a = np.array([10.0, np.nan, 4, np.nan, 2, 1])
         assert pm_nanmedian(a, 0) == 3, "nanmedian() on mixed numbers/NaN failed"
 
         ## all NaNs (fallback required)
-        a = np.array([np.nan, np.nan,  np.nan])
+        a = np.array([np.nan, np.nan, np.nan])
         assert pm_nanmedian(a, 0) == 0, "nanmedian() on all-NaN failed"
 
         ## empty array (fallback required)
